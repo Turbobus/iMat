5e97ec4c335056e0afde2b2cbd6cd7b505d35847
Pongag <vallemannen@live.se>
2021-05-07 16:06:25 +0200

    Changed name to log_in.fxml from intro.fxml.

--- src/iMat/Main.java
+++ src/iMat/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("intro.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("log_in.fxml"));
         primaryStage.setTitle("iMat");
         primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.show();
