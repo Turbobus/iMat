3a9be3295732536827c5790adefaa771867b9958
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-05-07 16:17:25 +0200

    Added blue card class and the fx id

---
+++ src/iMat/productCard_blue.java
@@ -0,0 +1,12 @@
+package iMat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class productCard_blue {
+
+    @FXML private AnchorPane blueCard;
+    @FXML private Button addToCartButtonCard;
+
+}
