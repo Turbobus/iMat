0f1109db63b1c59c11c6dcb4582ca5c07d824506
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-05-08 23:58:51 +0200

    Added fx id to the shopHolder and the dark overlay to be used

--- src/iMat/shopHolder.java
+++ src/iMat/shopHolder.java
@@ -5,9 +5,16 @@ import javafx.scene.layout.AnchorPane;
 
 public class shopHolder {
 
+    @FXML AnchorPane darkPane;      // The dark overlay
+    @FXML AnchorPane mainPane;      // Holder for all "component holder anchorpanes"
+
+
+    // Holders for individual components. Resides in mainPane above
     @FXML AnchorPane headerPane;
     @FXML AnchorPane categoryPane;
     @FXML AnchorPane gridPane;
     @FXML AnchorPane cartPane;
+
+
 
 }
