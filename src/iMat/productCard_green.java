97ecbc011f455e85b9023c1e09ae3d9d45345c94
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-05-08 23:25:20 +0200

    Added fx id to the elements in the green card

--- src/iMat/productCard_green.java
+++ src/iMat/productCard_green.java
@@ -1,4 +1,10 @@
 package iMat;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class productCard_green {
+    @FXML private Button minusCardButton;
+    @FXML private Button plusCardButton;
+    @FXML private Button amountTextCard;
 }
