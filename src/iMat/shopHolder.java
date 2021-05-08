849b81c796b5cc39acc148210237ba78a574e367
Turbobus <59963570+Turbobus@users.noreply.github.com>
2021-05-08 23:47:55 +0200

    Added a holder for the components of main shop view

---
+++ src/iMat/shopHolder.java
@@ -0,0 +1,13 @@
+package iMat;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class shopHolder {
+
+    @FXML AnchorPane headerPane;
+    @FXML AnchorPane categoryPane;
+    @FXML AnchorPane gridPane;
+    @FXML AnchorPane cartPane;
+
+}
