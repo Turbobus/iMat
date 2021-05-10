44893b59e2ae6baecc10426beb0094bffaff5d82
stiha <stihan01@gmail.com>
2021-05-10 15:41:26 +0200

    payConfirmation class

---
+++ src/iMat/PayConfirmation.java
@@ -0,0 +1,18 @@
+package iMat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class PayConfirmation {
+    @FXML private AnchorPane payConfirmation;
+    @FXML private Label purchaseInformationLbl; //den text som visar antal varor och totalt pris
+    @FXML private Button cancelPurchaseButton;
+    @FXML private Button confirmpurchaseButton;
+    @FXML private ImageView closeImgView;
+
+
+
+}
