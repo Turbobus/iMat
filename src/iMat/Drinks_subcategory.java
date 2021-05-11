b917d1cf861862d74e6a90902f20bdac073d234c
Pongag <vallemannen@live.se>
2021-05-11 15:22:24 +0200

    Made all buttons turn green when pressed

--- src/iMat/Drinks_subcategory.java
+++ src/iMat/Drinks_subcategory.java
@@ -1,10 +1,15 @@
 package iMat;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class Drinks_subcategory extends Subcategory {
+
+    @FXML private Button coldDrinksButton;
+    @FXML private Button warmDrinksButton;
 
     public Drinks_subcategory(shopHolder pController) {
 
@@ -19,5 +24,17 @@ public class Drinks_subcategory extends Subcategory {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    @FXML
+    private void onColdDrinksClicked() {
+        System.out.println("Kalla drycker");
+        coldDrinksButton.setId("subcategory_pressed_buttons");
+    }
+
+    @FXML
+    private void onWarmDrinksClicked() {
+        System.out.println("Varma drycker");
+        warmDrinksButton.setId("subcategory_pressed_buttons");
     }
 }
--- src/iMat/Subcategory.java
+++ src/iMat/Subcategory.java
@@ -1,17 +1,26 @@
 package iMat;
 
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
+import java.awt.*;
+
 
 public abstract class Subcategory extends AnchorPane {
 
     private final shopHolder pController;
+
+    @FXML private Button showAllButton;
 
     public Subcategory(shopHolder pController) {
 
         this.pController = pController;
     }
 
+    @FXML
+    private void onShowAllClicked() {
+        System.out.println("Visa alla");
+        showAllButton.setId("subcategory_pressed_buttons");
+    }
 }
