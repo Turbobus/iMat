86472ccb013a09de891f5df632e7f12afd3a0a3a
stiha <stihan01@gmail.com>
2021-05-18 15:06:23 +0200

    made earlierpurchases menu work correctly

--- src/iMat/SingularPurchase.java
+++ src/iMat/SingularPurchase.java
@@ -3,14 +3,22 @@ package iMat;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.PointLight;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import se.chalmers.cse.dat216.project.Order;
+import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SingularPurchase extends AnchorPane{
+
+    @FXML private AnchorPane singularPurchaseCard;
     //green version
     @FXML private Label gDateLabel;
     @FXML private Label gAmountLabel;
@@ -18,6 +26,7 @@ public class SingularPurchase extends AnchorPane{
     @FXML private Button gTakeOutCart;
     @FXML private AnchorPane gSingularPurchaseBack;
     @FXML private AnchorPane gSingularPurchaseFront;
+    @FXML private FlowPane gProductFlowPane;
 
     //blue version
     @FXML private Button bShowProducts;
@@ -27,10 +36,16 @@ public class SingularPurchase extends AnchorPane{
     @FXML private FlowPane bAllProductsPane;
     @FXML private AnchorPane bSingularPurchaseBack;
     @FXML private AnchorPane bSingularPurchaseFront;
+    @FXML private FlowPane bProductFlowPane;
 
-    private Controller controller;
+    private boolean expanded = false;
 
-    public SingularPurchase(Controller controller){
+
+    private EarlierPurchases pController;
+    private Order order;
+    private ArrayList<ProductItem> items = new ArrayList<>();
+
+    public SingularPurchase(EarlierPurchases controller, Order order){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("singularPurchase.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -41,17 +56,124 @@ public class SingularPurchase extends AnchorPane{
             throw new RuntimeException(exception);
         }
 
-        this.controller = controller;
+        this.pController = controller;
+        this.order = order;
+        setUp();
     }
 
-    /*
-    private void putAllInCart(ActionEvent event){
+
+    private void setUp(){
+        //green card
+        //gDateLabel.setText(order.getDate().toString());
+        //gAmountLabel.setText(order.get);
+
+        //blue card
+        //bDateLabel.setText(order.getDate().toString());
+        //bAmountLabel;
+
+        for(int i = 0;i<order.getItems().size();i++){
+            ProductItem product = new ProductItem(order.getItems().get(i), this);
+            items.add(product);
+        }
+
+    }
+
+
+    public void checkAllInCart(){
+        for(int i = 0;i<items.size();i++){
+            if(!(items.get(i).isInCart())){
+                return;
+            }
+        }
+        putAllInCart();
+    }
+
+    public void checkAllOutOfCart(){
+        for(int i = 0;i<items.size();i++){
+            if(items.get(i).isInCart()){
+                return;
+            }
+        }
+        takeOutOfCart();
+    }
+
+    public void putAllInCart(){
+        gProductFlowPane.getChildren().clear();
+        gSingularPurchaseBack.setPrefHeight(bSingularPurchaseBack.getPrefHeight());
         gSingularPurchaseBack.toFront();
-        gSingularPurchaseFront.toFront();
+        for (ProductItem s :  items)
+        {
+            s.inCart();
+        }
+        if(expanded){
+            addProductItems(gProductFlowPane);
+        }
 
     }
 
-     */
+    @FXML
+    public void setButtonPutAllInCart(ActionEvent event){
+        putAllInCart();
+
+    }
+
+    public void takeOutOfCart(){
+        bProductFlowPane.getChildren().clear();
+        bSingularPurchaseBack.setPrefHeight(gSingularPurchaseBack.getPrefHeight());
+        bSingularPurchaseBack.toFront();
+        for (ProductItem s :  items)
+        {
+            s.outOfCart();
+        }
+        if(expanded){
+            addProductItems(bProductFlowPane);
+        }
+
+
+    }
+
+    @FXML void setButtonTakeOutOfCart(ActionEvent event){
+        takeOutOfCart();
+    }
+
+    private void hideProducts(){
+        bSingularPurchaseBack.setPrefHeight(130);
+        gSingularPurchaseBack.setPrefHeight(130);
+        singularPurchaseCard.setPrefHeight(130);
+        expanded = false;
+    }
+    private void addItems(FlowPane fPane, AnchorPane aPane){
+        bProductFlowPane.getChildren().clear();
+        gProductFlowPane.getChildren().clear();
+
+        if(expanded){
+            hideProducts();
+        }
+        else{
+            singularPurchaseCard.setPrefHeight(180+78*(order.getItems().size()));
+            aPane.setPrefHeight(180+78*(order.getItems().size()));
+            addProductItems(fPane);
+            expanded = true;
+
+        }
+    }
+
+    private void addProductItems(FlowPane pane){
+        pane.getChildren().clear();
+        for(int i = 0;i<items.size();i++){
+            pane.getChildren().add(items.get(i));
+        }
+    }
+
+    public void showProducts(ActionEvent event){
+        addItems(bProductFlowPane, bSingularPurchaseBack);
+    }
+
+    public void gShowProducts(ActionEvent event){
+        addItems(gProductFlowPane, gSingularPurchaseBack);
+    }
+
+
 
 
 
