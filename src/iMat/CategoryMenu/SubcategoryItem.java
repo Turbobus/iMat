21beac0576dcd82cb53cbcf51306d6700f2b1ff0
Pongag <vallemannen@live.se>
2021-05-18 13:47:53 +0200

    Now all subcategories are clickable except "Visa alla"-buttons

--- src/iMat/CategoryMenu/SubcategoryItem.java
+++ src/iMat/CategoryMenu/SubcategoryItem.java
@@ -43,8 +43,29 @@ public class SubcategoryItem extends AnchorPane {
 
         List<Product> pc = null;
         switch (this.subcategoryButton.getText()) {
+            case "Drycker kalla" -> pc = database.getCategoryProducts(ProductCategory.COLD_DRINKS);
+            case "Drycker varma" -> pc = database.getCategoryProducts(ProductCategory.HOT_DRINKS);
+
+            case "Bär" -> pc = database.getCategoryProducts(ProductCategory.BERRY);
+            case "Kål" -> pc = database.getCategoryProducts(ProductCategory.CABBAGE);
+            case "Potatis, ris" -> pc = database.getCategoryProducts(ProductCategory.POTATO_RICE);
+            case "Örtkryddor" -> pc = database.getCategoryProducts(ProductCategory.HERB);
+
             case "Fisk" -> pc = database.getCategoryProducts(ProductCategory.FISH);
             case "Kött" -> pc = database.getCategoryProducts(ProductCategory.MEAT);
+
+            case "Baljväxter" -> pc = database.getCategoryProducts(ProductCategory.POD);
+            case "Mjöl, socker, salt" -> pc = database.getCategoryProducts(ProductCategory.FLOUR_SUGAR_SALT);
+            case "Nötter & frön" -> pc = database.getCategoryProducts(ProductCategory.NUTS_AND_SEEDS);
+            case "Pasta" -> pc = database.getCategoryProducts(ProductCategory.PASTA);
+
+            case "Citrusfrukter" -> pc = database.getCategoryProducts(ProductCategory.CITRUS_FRUIT);
+            case "Exotiska frukter" -> pc = database.getCategoryProducts(ProductCategory.EXOTIC_FRUIT);
+            case "Grönsaksfrukter" -> pc = database.getCategoryProducts(ProductCategory.VEGETABLE_FRUIT);
+            case "Meloner" -> pc = database.getCategoryProducts(ProductCategory.MELONS);
+            case "Rotfrukter" -> pc = database.getCategoryProducts(ProductCategory.ROOT_VEGETABLE);
+            case "Stenfrukter" -> pc = database.getCategoryProducts(ProductCategory.FRUIT);
+
             case "Visa alla" -> showAllEvent();
         }
 
